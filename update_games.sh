b29ce2d0948e0c65331c7c013da7c455d5baef35
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2024-04-25 22:34:20 +0200

    change dirs

--- update_games.sh
+++ update_games.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
 sudo apt install wget unzip
-wget https://github.com/emmauscollege/emmaus-arcade-games/archive/refs/heads/main.zip -O /home/arcade/Download/emmaus-arcade-games.zip
-unzip /home/arcade/Download/emmaus-arcade-games.zip
-rm -rf /home/arcade/Download/emmaus-arcade-games.zip
+wget https://github.com/emmauscollege/emmaus-arcade-games/archive/refs/heads/main.zip -O ~/Downloads/emmaus-arcade-games-main.zip
+unzip ~/Downloads/emmaus-arcade-games-main.zip
+rm -rf ~/Downloads/emmaus-arcade-games-main.zip
