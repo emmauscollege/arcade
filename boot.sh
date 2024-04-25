5c7520e013edbeab262d9192efd3cf90abdc2585
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2024-04-25 23:04:35 +0200

    http server to background

--- boot.sh
+++ boot.sh
@@ -6,13 +6,13 @@ if ping -c 1 8.8.8.8 &> /dev/null; then
     echo "updating rapberry pi OS..."
     #not implemented yet
     echo "Updating games..."
-    .~/emmaus-arcade-games-main/update_games.sh
+    ~/emmaus-arcade-games-main/update_games.sh
 else
     echo "No internet. Using locally stored games..."
 fi
 
 echo "starting webserver"
-python -m http.server 8000 --directory /home/arcade/emmaus-arcade-games-main/ 
+python -m http.server 8000 --directory /home/arcade/emmaus-arcade-games-main/ &
 
 echo "starting back to main menu deamon"
 # not implemented yet
