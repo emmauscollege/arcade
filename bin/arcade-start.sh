cb62342ef58f7b921933072568dd58997c624aa2
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2024-04-28 12:41:55 +0200

    kill webbrowser before start

--- bin/arcade-start.sh
+++ bin/arcade-start.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-# kill webserver process
-# not implemented yet
+echo "Stopping webserver if it was running"
+kill $(ps aux | grep 'http.server' |grep -v 'grep'| awk '{print $2}')
+# Details on the workings of previous command are as follows:
+# The ps gives you the list of all the processes.
+# The grep filters that based on your search string, grep -v returns all lines except the one(s) matched
+# The awk just gives you the second field of each line, which is the PID.
+# The $(x) construct means to execute x then take its output and put it on the command line. The output of that ps pipeline inside that construct above is the list of process IDs so you end up with a command like kill 1234 1122 7654.
+
 echo "Starting webserver"
 python -m http.server 8000 --directory ~/web/ &
 
-# kill deamon process
-# not implemented yet
-echo "Starting back to main menu deamon"
+echo "Stopping hotkey deamon if it was running"
 # not implemented yet
 
-# kill browser process
+echo "Starting hotkey deamon"
 # not implemented yet
+
+echo "Stopping webbrowser if it was running"
+kill $(ps aux | grep 'firefox' |grep -v 'grep'| awk '{print $2}')
+# Details on the workings of previous command are as follows:
+# The ps gives you the list of all the processes.
+# The grep filters that based on your search string, grep -v returns all lines except the one(s) matched
+# The awk just gives you the second field of each line, which is the PID.
+# The $(x) construct means to execute x then take its output and put it on the command line. The output of that ps pipeline inside that construct above is the list of process IDs so you end up with a command like kill 1234 1122 7654.
+
 
 echo "Starting webbrowser"
 # chromium doesn't work (some bug it seems)
