dedea49cdf7c8afbf2bae5efcaab8d41dd3ff7b1
vangeest <github@familievangeest.nl>
2024-05-03 21:41:29 +0000

    typo

--- arcade-install.sh
+++ arcade-install.sh
@@ -7,7 +7,7 @@ if ping -c 1 8.8.8.8 &> /dev/null; then
     echo "Updating rapberry pi OS..."
     sudo apt -q update
     sudo apt -q install wget unzip 
-    sudo apt -yq install unclutter 
+    sudo apt -yq install unclutter # did this manually as it didn't seem to work somehouw
     sudo apt -yq install python3-pynput
     sudo apt -yq install python3-evdev
 
