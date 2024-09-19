e3f078542b045d7cf10777ae55e71ea194eb0063
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2024-09-19 23:03:34 +0200

    try fix sound continues after stopping game

--- bin/arcade-home.sh
+++ bin/arcade-home.sh
@@ -9,6 +9,14 @@ kill $(ps aux | grep 'chromium-browser' |grep -v 'grep'| awk '{print $2}')
 # The awk just gives you the second field of each line, which is the PID.
 # The $(x) construct means to execute x then take its output and put it on the command line. The output of that ps pipeline inside that construct above is the list of process IDs so you end up with a command like kill 1234 1122 7654.
 
+# additional commands in an attempt to really kill browser processes
+# the sound continueing after previous commands let to the assumption that the previous command is not working
+pkill -KILL chromium
+pkill -KILL chrome
+pkill -KILL browser
+pkill -9 chromium
+pkill -9 chrome
+pkill -9 browser
 
 echo "Starting webbrowser"
 #firefox --kiosk http://localhost:8000/ &
