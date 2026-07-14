9ebace0984fa1dc38dc91e107612c6680ef163fb
SEG-EMC <seg@emmauscollege.nl>
2026-07-14 11:36:17 +0200

    Remove pop up

--- bin/arcade-home.sh
+++ bin/arcade-home.sh
@@ -4,4 +4,4 @@ echo "Stopping webbrowser if it was running"
 pkill -KILL chromium
 
 echo "Starting webbrowser"
-chromium-browser --noerrdialogs --disable-infobars --display=:0 --window-position=0,0 --incognito --kiosk http://localhost:8000/
+chromium-browser --noerrdialogs --disable-infobars --display=:0 --window-position=0,0 --incognito --password-store=basic --kiosk http://localhost:8000/
